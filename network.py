b68b9c46f3b90d93059a933e829b2d8e9ba27602
MakeWater <454156982@qq.com>
2018-12-28 10:05:53 +0800

    network file delete summary ops and add dropout layer
    network文件中的有关summary的操作都注释掉；增加了dropout层；更新了实验说明的备注。

--- network.py
+++ network.py
@@ -18,8 +18,8 @@ class siamese():
             with tf.name_scope('y_input'):
                 self.y_true = tf.placeholder(tf.float32,[None])
 
-            # with tf.name_scope('dropout'):
-                # self.dropout = tf.placeholder(tf.float32)
+            with tf.name_scope('dropout'):
+                self.dropout = tf.placeholder(tf.float32)
             with tf.name_scope('learning_rate'):
                 self.learning_rate = tf.placeholder(tf.float32,[None])
 
@@ -32,7 +32,8 @@ class siamese():
         
         with tf.name_scope('loss'):
             self.loss = self.contro_loss()
-            # tf.summary.scalar('loss',self.loss)
+
+
     def deepnn(self,x):
 
         # input reshape to [batch_size,28,28,channel]
@@ -40,16 +41,16 @@ class siamese():
             # transform input type to tensorflow type
             # x = tf.cast(x,tf.float32)
             x_image = tf.reshape(x,[-1,28,28,1])
-            tf.summary.image('input',x_image,10)
+            # tf.summary.image('input',x_image,10)
 
         # layer1: picture width = 28->28
         with tf.name_scope('conv1'):
             w_conv1 = self.weight_variable([5,5,1,32])
-            self.variable_summaries(w_conv1)
+            # self.variable_summaries(w_conv1)
             b_conv1 = self.bias_variable([32])
-            self.variable_summaries(b_conv1)
+            # self.variable_summaries(b_conv1)
             h_conv1 = tf.nn.relu(self.conv2d(x_image,w_conv1) + b_conv1)
-            tf.summary.histogram('activation1',h_conv1)
+            # tf.summary.histogram('activation1',h_conv1)
 
         # pooling layer1 : 28->14
         with tf.name_scope('pooling1'):
@@ -58,11 +59,11 @@ class siamese():
         # convolution layer2 : 14->14
         with tf.name_scope('conv2'):
             w_conv2 = self.weight_variable([5,5,32,64])
-            self.variable_summaries(w_conv2)
+            # self.variable_summaries(w_conv2)
             b_conv2 = self.weight_variable([64])
-            self.variable_summaries(b_conv2)
+            # self.variable_summaries(b_conv2)
             h_conv2 = tf.nn.relu(self.conv2d(h_pooling_1,w_conv2) + b_conv2)
-            tf.summary.histogram('activation2',h_conv2)
+            # tf.summary.histogram('activation2',h_conv2)
 
         # feature width : 14->7
         with tf.name_scope('pooling2'):
@@ -71,24 +72,24 @@ class siamese():
         with tf.name_scope('fc1'):
             # W2 = (W1-F+2P)/S + 1
             w_fc1 = self.weight_variable([7*7*64,1024])
-            self.variable_summaries(w_fc1)
+            # self.variable_summaries(w_fc1)
             b_fc1 = self.bias_variable([1024])
-            self.variable_summaries(b_fc1)
+            # self.variable_summaries(b_fc1)
             h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
             h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,w_fc1) + b_fc1)
-            tf.summary.histogram('activation3',h_fc1)
+            # tf.summary.histogram('activation3',h_fc1)
 
-        # with tf.name_scope('dropout'):
-            # h_fc1_drop = tf.nn.dropout(h_fc1,self.dropout)
+        with tf.name_scope('dropout'):
+            h_fc1_drop = tf.nn.dropout(h_fc1,self.dropout)
 
         with tf.name_scope('fc2'):
             # embedding in shape: [batch_size,10]
             w_fc2 = self.weight_variable([1024,10])
-            self.variable_summaries(w_fc2)
+            # self.variable_summaries(w_fc2)
             b_fc2 = self.bias_variable([10])
-            self.variable_summaries(b_fc2)
-            embedding = tf.matmul(h_fc1,w_fc2)+b_fc2
-            tf.summary.histogram('embedding',embedding)
+            # self.variable_summaries(b_fc2)
+            embedding = tf.matmul(h_fc1_drop,w_fc2)+b_fc2
+            # tf.summary.histogram('embedding',embedding)
             # embedding = tf.nn.softmax(embedding,dim=1)
             # tf.reshape(embedding,[1000,10])
         return embedding
