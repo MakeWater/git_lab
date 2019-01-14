51602c71cfa93346b82cb52cee1d2d3f4d0f5501
MakeWater <454156982@qq.com>
2019-01-14 11:51:59 +0800

    Update run.py

--- run.py
+++ run.py
@@ -47,11 +47,11 @@ siam = siamese()
 # loss = tf.losses.cosine_distance(simi,y_,axis=0)
 # loss = contro_loss(left_output,right_output,y_)
 # loss = contrastive_loss(left_output,right_output,y_,margin=0.5)
-for batch_size in [4,8,16,32]:
+for batch_size in [128,512]:
     global_step = tf.Variable(0,trainable=False) #只有变量（variable）才要初始化，张量（Tensor）是没法初始化的
     with tf.name_scope('learning_rate'):
         learning_rate_0 = tf.Variable(0.1,name='initial_lr')
-        learning_rate_decay_steps = 99000/batch_size
+        learning_rate_decay_steps = 68000/batch_size
         learning_rate = tf.train.exponential_decay(learning_rate_0,global_step,learning_rate_decay_steps,0.96) # 每喂入100个batch_size的数据后学习率衰减到最近一次的96%。
         # tf.summary.scalar('learning_rate',learning_rate)
 
@@ -67,8 +67,8 @@ for batch_size in [4,8,16,32]:
             sess.run(tf.global_variables_initializer())
             # merged = tf.summary.merge_all()
             # pairs, pairs_label, class_indices, index_to_pair, label_pred = get_pairs_by_None(unlabel_data,params)
-            pairs = np.load('pairs_raw.npy').astype(np.float32)
-            pairs_label = np.load('pairs_raw_label.npy').astype(np.float32)
+            pairs = np.load('pairs.npy').astype(np.float32)
+            pairs_label = np.load('pairs_label.npy').astype(np.float32)
             # pairs = pairs[:1000]
             # pairs_label = pairs_label[:1000]
             # NMI_score = NMI(label_pred,label)
